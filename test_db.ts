382f962fb5d3565858d5e24175a9a001b0a4ab68
edghardanu <139091412+edghardanu@users.noreply.github.com>
2026-08-14 11:19:56 +0700

    Delete test_db.ts

--- test_db.ts
+++
@@ -1,1 +0,0 @@
-import { db } from './src/lib/db.ts'; import { sellerProfiles } from './src/lib/schema.ts'; async function main() { const u = await db.select({storeName: sellerProfiles.storeName}).from(sellerProfiles); console.log(u.map(x => x.storeName)); } main();
