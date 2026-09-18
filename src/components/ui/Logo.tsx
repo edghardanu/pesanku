fc78c000054a264dbfde17aa89d4a4662ed6fcd3
Edghar Danuwijaya <email_anda@example.com>
2026-09-19 02:58:46 +0700

    update

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,34 +1,39 @@
 import React from 'react';
 
 interface LogoProps {
+  src?: string;
   className?: string;
   width?: number;
   height?: number;
   priority?: boolean;
 }
 
-export default function Logo({ className = '', width, height }: LogoProps) {
+export default function Logo({ src, className = '', width, height }: LogoProps) {
   const customStyle = width && height ? { width: `${width}px`, height: `${height}px` } : undefined;
+
+  const lightSrc = src || "/pesanku-logo-light.png";
+  const darkSrc = src || "/pesanku-logo-dark.png";
 
   return (
     <div className={`relative flex items-center justify-center shrink-0 ${className}`} style={customStyle}>
       {/* Light Mode Logo */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/pesanku-logo-light.png"
+        src={lightSrc}
         alt="Pesanku Nusantara Logo"
         className="block dark:hidden w-full h-auto object-contain"
       />
       {/* Dark Mode Logo */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/pesanku-logo-dark.png"
+        src={darkSrc}
         alt="Pesanku Nusantara Logo"
         className="hidden dark:block w-full h-auto object-contain"
       />
     </div>
   );
 }
+
 
 
 
